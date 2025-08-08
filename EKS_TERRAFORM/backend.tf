e5fc04741c4f3734f302004e1d4a14a4fd874bdf
Umeshpardeshi12 <umesh01pardeshi@gmail.com>
2025-08-08 17:21:46 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "myprojects3bucket-hotstar_clone" # Replace with your actual S3 bucket name
+    bucket = "bucket_22emcjdmfjfn" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
